a578fa03b1d99aa3d8d4abbc811174d19d92fe74
emonShimoul <emonshimoul@gmail.com>
2022-07-01 12:34:24 +0600

    use post method for send the cart data to the server

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,28 +1,41 @@
 import { useEffect, useState } from "react"
 import { getStoredCart } from "../utilities/fakedb";
 
-const useCart = products => {
+const useCart = () => {
     const [cart, setCart] = useState([]);
     // console.log(cart);
     useEffect(() => {
-        if(products.length){
-            const savedCart = getStoredCart();
-            // console.log("Saved Cart: ",savedCart);
-            const storedCart = [];
-            for(const key in savedCart){
-                const addedProduct = products.find(product => product.key == key);
-                // console.log(addedProduct);
-                if(addedProduct){
-                    // set quantity
-                    const quantity = savedCart[key];
-                    addedProduct.quantity = quantity;
-                    storedCart.push(addedProduct);
-                    // console.log(storedCart);
-                }
-            }
-            setCart(storedCart);
-        }
-    }, [products]);
+        const savedCart = getStoredCart();
+        const keys = Object.keys(savedCart);
+        fetch('http://localhost:5000/products/byKeys', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(keys)
+        })
+        .then(res => res.json())
+        .then(products => {
+            console.log(products);
+            // if(products.length){
+            //     // console.log("Saved Cart: ",savedCart);
+            //     const storedCart = [];
+            //     for(const key in savedCart){
+            //         const addedProduct = products.find(product => product.key == key);
+            //         // console.log(addedProduct);
+            //         if(addedProduct){
+            //             // set quantity
+            //             const quantity = savedCart[key];
+            //             addedProduct.quantity = quantity;
+            //             storedCart.push(addedProduct);
+            //             // console.log(storedCart);
+            //         }
+            //     }
+            //     setCart(storedCart);
+            // }
+        })
+        
+    }, []);
     return [cart, setCart];
 }
 
